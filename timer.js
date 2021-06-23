47d131238506a2cae1db8d56c7caa4a9cce1871e
evg-zlg <85534817+evg-zlg@users.noreply.github.com>
2021-06-23 10:08:38 +0500

    Add files via upload

--- timer.js
+++ timer.js
@@ -52,11 +52,17 @@ function runWorkout() {
             document.getElementById('run').className = 'restart'
             document.getElementById('run').textContent = 'Начать сначала'
             document.getElementById('stop').style = 'visibility: visible;' +
-                'width: 50mm; height: 30mm; font-size: 200%;'  
+                'width: 50mm; height: 30mm; font-size: 200%; ' +
+                'position: fixed; top: 0; left: 0'  
+            document.getElementById('circles').style = 'position: fixed; top: 30mm'
+            document.getElementById('run').style = 'position: fixed; top: 100mm; width: 50mm'
+
             document.getElementById('cPlus').style = 'visibility: hidden;'
             document.getElementById('cMinus').style = 'visibility: hidden;'
             document.getElementById('tPlus').style = 'visibility: hidden;'
             document.getElementById('tMinus').style = 'visibility: hidden;'
+            document.getElementById('baseinf').style = 'visibility: hidden;'
+
 
             printCircles(circles);
             step = 0;
@@ -71,6 +77,7 @@ function runWorkout() {
             document.getElementById('cMinus').style = 'visibility: visible; width: 10mm;'
             document.getElementById('tPlus').style = 'visibility: visible; width: 10mm;'
             document.getElementById('tMinus').style = 'visibility: visible; width: 10mm;'
+            document.getElementById('baseinf').style = 'visibility: visible;'
             document.getElementById('timer').textContent = timerGlob;
             document.getElementById('count').textContent = countGlob;
             timer = timerGlob
@@ -118,13 +125,13 @@ function runWorkout() {
             document.getElementById('timer').textContent = timerGlob
             }
     })
-    
+    // нажали прибавить время
     let tPlusButton = document.getElementById('tPlus');
     tPlusButton.addEventListener('click', cel => {
         timerGlob = timerGlob +5;
         document.getElementById('timer').textContent = timerGlob
     })
-    
+    // нажали убавить время
     let tMinusButton = document.getElementById('tMinus');
     tMinusButton.addEventListener('click', cel => {
         timerGlob = timerGlob -5;
